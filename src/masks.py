81bd9d8ffccedf6dfc2dbc9251ff5094249f760b
Vladimir Mykalo <vladimirwork883@yandex.ru>
2025-05-07 09:47:22 +0500

    last_control

--- src/masks.py
+++ src/masks.py
@@ -13,7 +13,7 @@ def get_mask_card_number(card_number: str) -> str:
     """Функция вводит номер карты, проверяет и выводит маску карты"""
     card_number = card_number.replace(" ", "")
     # проверяем хватает цифр в номере карты
-    print(card_number)
+
     if len(card_number) != 16 or not card_number.isdigit():
         # проверяем правильно ли набран номер карты и все ли символы цифры
         return "Введен неверный номер карты"
--- src/widget.py
+++ src/widget.py
@@ -1,18 +1,28 @@
 from src.masks import get_mask_account, get_mask_card_number
-#import masks
+
+
 def mask_account_card(number_type: str) -> str:
     """ Функция принимает тип и номер карты или счета и возвращает строку с замаскированным номером"""
-    length_name_card_or_account=0
-    for char  in number_type:
+    length_name_card_or_account = 0
+    for char in number_type:
         if not char.isdigit():
             length_name_card_or_account += 1
-    print(len(number_type))
-    print (length_name_card_or_account)
-    if "Счет" or "счет" in number_type:
+
+    if "Счет" in number_type or "счет" in number_type:
         number_mask = get_mask_account(number_type[length_name_card_or_account:])
-        return f"".join(number_type[0:length_name_card_or_account]+number_mask)
+        return f"".join(number_type[0:length_name_card_or_account] + number_mask)
     else:
         number_mask = get_mask_card_number(number_type[length_name_card_or_account:])
-        return f"".join({number_type[0:length_name_card_or_account]+number_mask})
-number_type=input()
+        return f"".join(number_type[0:length_name_card_or_account] + number_mask)
+
+
+def get_date(user_date: str) -> str:
+    """Функция преобразует входную дату и время в короткий вариант"""
+
+    return f'{user_date[8:10]}.{user_date[5:7]}.{user_date[0:4]}'
+
+number_type = input()
 print(mask_account_card(number_type))
+
+user_date = input()
+print(get_date(user_date))

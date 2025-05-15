1f0d07b26c680cfbe5b0b4c0645065284493c711
Vladimir Mykalo <mykalovladimir883@gmail.com>
2025-05-15 19:52:12 +0500

    update_main.py_update_processing.py

--- main.py
+++ main.py
@@ -1,9 +1,29 @@
-import src.masks
+"""import src.masks
 import src.widget
+import src.processing
+"""
 
-#account_number = input("Введите номер счета\n")
-# Печатаем маску счета
-#print(src.masks.get_mask_account(account_number))
-# Печатаем маску карты
-#masked_card_result_with_masks = input("Введите номер карты\n")
-#print(src.masks.get_mask_card_number(masked_card_result_with_masks))
+from src.processing import filter_by_state, sort_by_date
+
+"""account_number = input("Введите номер счета\n")
+     Печатаем маску счета
+     print(src.masks.get_mask_account(account_number))
+     Печатаем маску карты
+     masked_card_result_with_masks = input("Введите номер карты\n")
+     print(src.masks.get_mask_card_number(masked_card_result_with_masks))
+     функции относятся к другим модулям и еще понадобятся в дальнейшем
+"""
+operations_list = [
+    {"id": 41428829, "state": "EXECUTED", "date": "2019-07-03T18:35:29.512364"},
+    {"id": 939719570, "state": "EXECUTED", "date": "2018-06-30T02:08:58.425572"},
+    {"id": 594226727, "state": "CANCELED", "date": "2018-09-12T21:27:25.241689"},
+    {"id": 615064591, "state": "CANCELED", "date": "2018-10-14T08:21:33.419441"},
+    {"id": 623454892, "state": "EXECUTED", "date": "2018-11-16T08:23:53.419441"},
+]
+
+filtered = filter_by_state(operations_list, "EXECUTED")
+
+sorted_operations = sort_by_date(filtered)
+
+for operation in sorted_operations:
+    print(operation)
--- src/widget.py
+++ src/widget.py
@@ -2,14 +2,14 @@ from src.masks import get_mask_account, get_mask_card_number
 
 
 def mask_account_card(number_type: str) -> str:
-    """ Функция принимает тип и номер карты или счета и возвращает строку с замаскированным номером"""
+    """Функция принимает тип и номер карты или счета и возвращает строку с замаскированным номером"""
     length_name_card_or_account = 0
     # В цикле определяем длину части строки не являющуюся номером счета
     for char in number_type:
         if not char.isdigit():
             length_name_card_or_account += 1
-
-    if "Счет" in number_type or "счет" in number_type:#определяем счет или карта
+    # определяем счет или карта
+    if "Счет" in number_type or "счет" in number_type:
         number_mask = get_mask_account(number_type[length_name_card_or_account:])
         return "".join(number_type[0:length_name_card_or_account] + number_mask)
     else:
@@ -19,7 +19,7 @@ def mask_account_card(number_type: str) -> str:
 
 def get_date(user_date: str) -> str:
     """Функция преобразует входную дату и время в короткий вариант"""
-    return f'{user_date[8:10]}.{user_date[5:7]}.{user_date[0:4]}'
+    return f"{user_date[8:10]}.{user_date[5:7]}.{user_date[0:4]}"
 
 
 number_type = input("Введите номер карты или счета")
